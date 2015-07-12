4f6f0a54737200a7d9834c775be7a14e184e6c48
Jan Graefe <jan.graefe@gmx.de>
2015-07-12 13:21:51 +0200

    XmlOutputNormalizer: Normalize to UTF8 xml output

--- WindowsServiceInformation/OutputNormalizer/XmlOutputNormalizer.cs
+++ WindowsServiceInformation/OutputNormalizer/XmlOutputNormalizer.cs
@@ -8,19 +8,37 @@ using System.Xml.Serialization;
 
 namespace ch.jaxx.WindowsServiceInformation
 {
+    /// <summary>
+    /// Normalization class for xml output
+    /// </summary>
     public class XmlOutputNormalizer : IOutputNormalizer
     {
         public List<OutputModel> Normalize(List<WindowsServiceInfo> ServiceInformationList)
-        {
-            XmlSerializer xsSubmit = new XmlSerializer(typeof(List<WindowsServiceInfo>));
-            var subReq = ServiceInformationList;
-            StringWriter sww = new StringWriter();
-            XmlWriter writer = XmlWriter.Create(sww);
-            xsSubmit.Serialize(writer, subReq);
+        {            
+            XmlSerializer serializer = new XmlSerializer(typeof(List<WindowsServiceInfo>));
 
-            var normalizedOutput = new OutputModel();
-            normalizedOutput.FileName = "x";
-            normalizedOutput.Content = new string[] { sww.ToString() };
+            // create a MemoryStream here, we are just working
+            // exclusively in memory
+            System.IO.Stream stream = new System.IO.MemoryStream();
+
+            // The XmlTextWriter takes a stream and encoding
+            // as one of its constructors
+            System.Xml.XmlTextWriter xtWriter = new System.Xml.XmlTextWriter(stream, Encoding.UTF8);
+
+            serializer.Serialize(xtWriter, ServiceInformationList);
+
+            xtWriter.Flush();
+
+            // go back to the beginning of the Stream to read its contents
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+
+            // read back the contents of the stream and supply the encoding
+            System.IO.StreamReader reader = new System.IO.StreamReader(stream, Encoding.UTF8);
+
+            string result = reader.ReadToEnd();
+            
+            var normalizedOutput = new OutputModel();            
+            normalizedOutput.Content = new string[] { result.ToString() };
             return new List<OutputModel>() { normalizedOutput };
             
         }
